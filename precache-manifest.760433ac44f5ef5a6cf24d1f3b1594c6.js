50b7663601b355724a3578f4fa50885199926322
Prince-Shivaram <princeshivaram1999@gmail.com>
2020-09-08 10:24:18 +0530

    Updates

---
+++ precache-manifest.760433ac44f5ef5a6cf24d1f3b1594c6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b57203beb79ab8a81075a4116a2a1df7",
+    "url": "/movies-nominator/index.html"
+  },
+  {
+    "revision": "54bc98edaa0cd5b4dc18",
+    "url": "/movies-nominator/static/css/main.51d09e12.chunk.css"
+  },
+  {
+    "revision": "4efd1b4bb9b560e88d8c",
+    "url": "/movies-nominator/static/js/2.510b544e.chunk.js"
+  },
+  {
+    "revision": "b8ccd9306ca3185ea687a4a38aaaf574",
+    "url": "/movies-nominator/static/js/2.510b544e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "54bc98edaa0cd5b4dc18",
+    "url": "/movies-nominator/static/js/main.5ea0f089.chunk.js"
+  },
+  {
+    "revision": "398d263ba0382e2cf19b",
+    "url": "/movies-nominator/static/js/runtime-main.af7567ad.js"
+  }
+]);
